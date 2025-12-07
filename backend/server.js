3eb29f517bba25b6614e8e371029e798bf3501b5
saiyedkounain <syedkounainlife@gmail.com>
2025-12-07 08:08:05 +0530

    set up backend

---
+++ backend/server.js
@@ -0,0 +1,30 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+
+const app = express();
+
+// middle ware to handle cors
+
+app.use(
+    cors({
+        origin: "http://localhost:3000",
+        methods: ["GET", "POST", "PUT", "DELETE"],
+        allowedHeaders: ["Content-Type", "Authorization"],
+    })
+);
+
+// the middleware
+app.use(express.json());
+
+//routes
+
+//serve uploads folder
+app.use('/uploads', express.static(path.join(__dirname, 'uploads'), {}));
+
+//start server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
